bbd21466c0a19ea1e8097e1a8f8ec4fbd9f9d645
renatasellanes <renatasellanes@hotmail.com>
2023-02-25 11:56:21 -0300

    fixed folders

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import connectDb from './config/db.connection.js'
+import agencyRouter from '../routes/agency.route.js'
+import commentsRouter from '../routes/comments.route.js'
+import exchangeRouter from '../routes/exchanges.route.js'
+import userRouter from '../routes/user.route.js'
+import authRouter from './routes/auth.routes.js'
+
+const app = express()
+connectDb()
+
+app.use(cors())
+app.use(express.json())
+app.use('/exchange', exchangeRouter)
+app.use('/user', userRouter)
+app.use('/comments', commentsRouter)
+app.use('/agency', agencyRouter)
+app.use(authRouter)
+
+app.listen(3001, () => console.log('Server listening on port ', 3001))
