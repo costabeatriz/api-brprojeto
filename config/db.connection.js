7dae795ed65ee57936bccfc4df8d0c1279e63d65
costabeatriz <113780157+costabeatriz@users.noreply.github.com>
2023-03-18 12:31:35 -0500

    db.connection

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    const connection = await mongoose.connect(process.env.MONGO_URI)
+    try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to mongo! Database: ', mongoose.connections[0].name)
+    } catch (error){
+        console.error(error);
+    }
+
+    
 }
 
 export default connectDb
